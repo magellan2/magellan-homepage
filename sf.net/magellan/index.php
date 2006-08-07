7f46b865137b4a82951a423c31c329ccb6e7a6a9
Enno Rehling <ennorehling@users.noreply.github.com>
2006-08-07 20:02:41 +0000

    import of existing magellan webpages.

---
+++ sf.net/magellan/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<title>Eressea PBEM</title>
+ <?php
+$acceptLang=(($_SERVER["HTTP_ACCEPT_LANGUAGE"] == '') ? '*' :
+						         $_SERVER["HTTP_ACCEPT_LANGUAGE"]);
+$alparts=@preg_split("/,/",$acceptLang);
+foreach($alparts as $part) {
+    $part=trim($part);
+    if(preg_match("/;/", $part)) {
+				$lang=@preg_split("/;/",$part);
+				$part=$lang[0];
+    }
+		if ($part == 'de') break;
+		if ($part == 'en') break;
+		$part = 'en';
+}
+echo "<meta http-equiv=\"refresh\"CONTENT=\"0;URL=$part/\">";
+?>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+<body bgcolor="#D5A983">
+<tt><a href="en/">Click here</a> to get to the english pages.<br>
+<a href="de/">Hier klicken</a> um zu den deutschen Seiten zu gelangen.<br>
+</tt>
+</body>
+</html>
