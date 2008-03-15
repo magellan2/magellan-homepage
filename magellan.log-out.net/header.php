f7f2ccde0fe7fdfd2416d3c9af461d4b9f805a2b
Thoralf Rickert-Wendt <30341294+trickert76@users.noreply.github.com>
2008-03-15 20:21:46 +0000

    misc

--- magellan.log-out.net/header.php
+++ magellan.log-out.net/header.php
@@ -1,5 +1,5 @@
       <div id="head">
-        <a href="." target="_top">
-          <img src="images/header.gif" alt="Magellan - Eressea Java-Client" border="0" onfocus="if(this.blur()){this.blur();}"/>
+        <a href="." target="_top" onfocus="if(this.blur()){this.blur();}">
+          <img src="images/header.gif" alt="Magellan - Eressea Java-Client" border="0"/>
         </a>
       </div>
