31a8bc3821d60cf9aab60805be16e5ab12c3a154
trickert <trickert@1c5abfc5-b992-4a1f-8ccd-9ce068e22774>
2008-03-21 17:41:59 +0000

    More texts on first page (copied from current website)

--- www/magellan.log-out.net/index_de.php
+++ www/magellan.log-out.net/index_de.php
@@ -19,6 +19,16 @@
       ?>
       <div id="center">
         <h3>
+          Was ist Magellan?
+        </h3>
+        <p>
+          Magellan ist ein vollständiger Client für 
+          <a href="http://www.eressea.de/" class="externalLink">Eressea</a>. 
+          Man kann damit seine Karte anzeigen, Suchen, Befehle geben, und 
+          überhaupt braucht man das Programm fast nur noch zu verlassen, 
+          um Mails an die Verbündeten zu schreiben. Fast.
+        </p>
+        <h3>
           Was ist Magellan 2.x?
         </h3>
         <p>
@@ -27,16 +37,16 @@
           uns entschlossen haben, eine neue Versionsnummer zu vergeben.
         </p>
         <p>
-          Magellan2 benötigt, im Gegensatz zu Magellan1 nun Java5 (1.5.0 oder
-          höher). Außerdem handelt es sich bei der angebotenen JAR Datei nun um
-          einen Setup-Programm, das den Client auspackt und auf Deinem PC
-          installiert. Das Programm ist aufgeteilt in viele Dateien. Das bietet
-          Euch recht einfach die Möglichkeit, Texte oder Icons an Eure
-          Bedürfnisse anzupassen.
-        </p>
-        <p>
           <a href="images/magellan2.png" onfocus="if(this.blur()){this.blur();}"><img src=
           "images/magellan2-preview.png" border="0" alt="Magellan Desktop"/></a>
+        </p>
+        <p>
+          Magellan2 benötigt, im Gegensatz zu Magellan1 nun Java5 (1.5.0 oder
+          höher). Außerdem handelt es sich bei der angebotenen JAR Datei nun um
+          ein Setup-Programm, das den Client auspackt und auf Deinem PC
+          installiert. Das Programm ist nun aufgeteilt in viele Dateien. Das bietet
+          Euch recht einfach die Möglichkeit, Texte oder Icons an Eure
+          Bedürfnisse anzupassen.
         </p>
         <p>
           Außerdem bietet Magellan2 noch einige Besonderheiten. Mit Hilfe des
@@ -46,6 +56,58 @@
           Darüberhinaus verfügt Magellan2 nun über eine getrennte
           Code-Struktur, um Server- oder Konsolen-basierte Anwendungen zu
           ermöglichen, ohne eine GUI Oberfläche haben zu müssen.
+        </p>
+        <h3>
+          Was ist Magellan nicht?
+        </h3>
+        <p>
+          Magellan ist nur eines von vielen 
+          <a href="http://www.eressea.de/de/downloads.shtml" class="externalLink">Client Programmen</a>
+          für Eressea und steht in keinem Zusammenhang mit dem Eresseateam. Bitte 
+          wende dich daher bei Fragen und Problemen an die 
+          <a href="feedback_de.php">Magellan Community</a>.
+        </p>
+        <h3>
+          Läuft Magellan auf meinem Rechner?
+        </h3>
+        <p>
+          Dank Java läuft Magellan auf MS Windows, Linux, MacOS X und diversen 
+          anderen Betriebssystemen. Benötigt wird allerdings ein etwas 
+          leistungsfähigerer Rechner, gerade wenn die Partei wächst. Die 
+          Mindestausstattung liegt bei einem Prozessor mit 450 MHz und 128 MB 
+          Hauptspeicher, empfehlenswert sind jedoch 800 MHz und 512 MB 
+          Hauptspeicher. Magellan läuft ab Java 1.5.
+        </p>
+        <h3>
+          Java? Das kann ich nicht!
+        </h3>
+        <p>
+          Kein Problem, das ist auch nicht nötig. Nach der einmaligen 
+          Installation von <a href="http://java.sun.com/javase/downloads/" 
+          class="externalLink">Java</a> braucht man nur noch Magellan 
+          <a href="download_de.php">herunterladen</a> - eine Datei, die man 
+          im Windows-Explorer per Doppelklick starten kann. Mit 
+          Programmieren in Java hat das alles nichts zu tun, damit müßen 
+          sich nur die Entwickler herumschlagen.
+        </p>
+        <h3>
+          Ist Magellan gut?
+        </h3>
+        <p>
+          Nun - immerhin konnte Magellan inzwischen Ennos <b>Seal of 
+          Excellence</b> einheimsen.<br/>
+          Laut der <b>Spielerumfrage Ende 2001</b> scheint Magellan außerdem das 
+          bei den Eressea Spielern beliebteste Tool zu sein.
+        </p>
+        <h3>
+          Wie sieht's mit der Weiterentwicklung aus?
+        </h3>
+        <p>
+          Magellan ist ein Open-Source Projekt, an dem sich mehrere Entwickler 
+          beteiligen. Dadurch ist sichergestellt, dass Anpassungen z.B. an 
+          Änderungen im CR immer sehr schnell umgesetzt werden. Durch Vorschläge 
+          und Hinweise können die User neue Ideen einbringen und Einfluss auf 
+          existierende Features nehmen. (<a href="feedback_de.php">Feedback</a>)
         </p>
       </div>
     </div>
--- www/magellan.log-out.net/index_en.php
+++ www/magellan.log-out.net/index_en.php
@@ -19,12 +19,26 @@
       ?>
       <div id="center">
         <h3>
+          What is Magellan?
+        </h3>
+        <p>
+          Magellan is a full-featured client tool for the play-by-e-mail game 
+          <a href="http://www.eressea.de/" class="externalLink">Eressea</a>. 
+          It displays a map of your part of the Eressea world, supports you 
+          in writing orders, performs many complex calculations for you and is 
+          available completely in English.
+        </p>
+        <h3>
           What is Magellan 2.x?
         </h3>
         <p>
           Magellan 2.x is a logical progression of the Magellan client. The
           changes in the software were so extensive that we decided to give
           it a new, much higher, version number.
+        </p>
+        <p>
+          <a href="images/magellan2.png" onfocus="if(this.blur()){this.blur();}"><img src=
+          "images/magellan2-preview.png" border="0" alt="Magellan Desktop"/></a>
         </p>
         <p>
           Magellan 2.x now needs, contrary to Magellan 1.x, Java5 (1.5.0 or
@@ -34,10 +48,6 @@
           change texts and icons as you want.
         </p>
         <p>
-          <a href="images/magellan2.png" onfocus="if(this.blur()){this.blur();}"><img src=
-          "images/magellan2-preview.png" border="0" alt="Magellan Desktop"/></a>
-        </p>
-        <p>
           Magellan 2.x also offers some new features. With the help of the
           Infonode Docking Framework you can now customize your Magellan
           desktop to your own tastes and needs with a single mouse click. You
@@ -45,6 +55,58 @@
           Magellan 2.x now has a split code structure, which makes it possible to
           develop server or console applications without a graphical user
           interface.
+        </p>
+        <h3>
+          What is Magellan not?
+        </h3>
+        <p>
+          Magellan is only one among many other <a href="http://www.eressea.de/de/downloads.shtml" 
+          class="externalLinks">client programs</a> for Eressea and is in no way 
+          related to the developers of Eressea. To 
+          relieve them of unnecessary work please contact the 
+          <a href="feedback_en.php">Magellan Community</a> in case you have any 
+          questions or problems.
+        </p>
+        <h3>
+          Can I Run Magellan on My Computer?
+        </h3>
+        <p>
+          Magellan is built using the platform independent Java technology and 
+          therefore runs on MS Windows, Linux, MacOS X and some other operating 
+          systems. However, it requires a rather powerful machine, especially 
+          when your faction grows bigger. The minimum system requirements are 
+          a 450 MHz processor and 128 MB RAM, but 800 MHz and 512 MB RAM are 
+          recommended. For Magellan Java 1.5 or newer is required.
+        </p>
+        <h3>
+          Java? What's That?
+        </h3>
+        <p>
+          Java is a platform independent programming system. Actually, you do 
+          not really have to worry about that. After installing 
+          <a href="http://java.sun.com/javase/downloads/" class="externalLink">Java</a>
+          on your computer once, all you need is to <a href="download_en.php">download</a>
+          Magellan.  Consisting only of a single file, you can run it like any other 
+          program from then on.
+        </p>
+        <h3>
+          Is Magellan a Quality Product?
+        </h3>
+        <p>
+          Well, for one thing, Magellan was able to achieve Enno's <b>Seal of 
+          Excellence</b>. According to the Eressea poll in December 2001 
+          Magellan seems to be by far the most popular tool among the Eressea 
+          players.
+        </p>
+        <h3>
+          Is Magellan Up-To-Date?
+        </h3>
+        <p>
+          Magellan is an open source project with a bunch of developers ensuring 
+          that adaptations to the latest Eressea features are always available 
+          quickly. Also, the users form an integral part of the Magellan 
+          development process by bringing in enhancement requests and new 
+          ideas: (<a href="feedback_en.php">Feedback</a>)
         </p>
       </div>
     </div>
