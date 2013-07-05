23490b5b461895a35c19f33a74ff180e9a450572
stm2 <stm2@1c5abfc5-b992-4a1f-8ccd-9ce068e22774>
2013-07-05 12:29:28 +0000

    TeachPlugin: new published version 0.15 for nightly

--- magellan.log-out.net/plugins_teacher_en.php
+++ magellan.log-out.net/plugins_teacher_en.php
@@ -38,7 +38,7 @@
               Version:
             </td>
             <td class="plugin">
-              0.10.5
+              0.10.5/0.15
             </td>
           </tr>
           <tr class="plugin">
